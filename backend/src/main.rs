de3def425e92504ef661ddcbf545722e2517d889
f4r3n <guillaume_k@hotmail.fr>
2025-05-24 22:11:59 +0200

    fix listening url

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,4 +1,4 @@
-use std::env;
+use std::{env, str::FromStr};
 pub mod entrypoint;
 pub mod models;
 pub mod schema;
@@ -18,19 +18,20 @@ async fn main() -> anyhow::Result<()> {
     let state_server = state_server::StateServer { pool: connection };
     let front_url = env::var("FRONT_URL")?;
 
-    let port = env::var("PORT")?.parse::<u16>()?;
+    let listening_url = env::var("LISTENING_URL")?;
     let app: Router = router::create_router(&front_url, state_server)?;
     let backend = async {
-        let _ = serve(app, port).await;
+        let _ = serve(app, &listening_url).await;
     };
 
     tokio::join!(backend);
     Ok(())
 }
 
-async fn serve(app: Router, port: u16) -> anyhow::Result<()> {
-    let addr = SocketAddr::from(([127, 0, 0, 1], port));
-    println!("Backend run: 127.0.0.1:{}", port);
+async fn serve(app: Router, listening_url: &str) -> anyhow::Result<()> {
+    println!("Backend run: {}", listening_url);
+
+    let addr = SocketAddr::from_str(listening_url)?;
     let listener = tokio::net::TcpListener::bind(addr).await?;
     axum::serve(listener, app).await?;
     Ok(())
