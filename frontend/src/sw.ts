72e9d6e578d3972e1c7c707980b5d3e215877ff3
F4r3n <guillaume_k@hotmail.fr>
2025-06-06 18:26:16 +0100

    remove fallback

--- frontend/src/sw.ts
+++ frontend/src/sw.ts
@@ -1,13 +1,4 @@
-import { precacheAndRoute, createHandlerBoundToURL  } from 'workbox-precaching';
-import { registerRoute } from 'workbox-routing';
+import { precacheAndRoute  } from 'workbox-precaching';
 declare let self: ServiceWorkerGlobalScope;
 
 precacheAndRoute(self.__WB_MANIFEST);
-
-// App Shell-style routing for SPA navigation
-registerRoute(
-  // Check if this is a navigation request
-  ({ request, }) => request.mode === 'navigate',
-  // Serve the app shell (index.html or /)
-  createHandlerBoundToURL('/')
-);
